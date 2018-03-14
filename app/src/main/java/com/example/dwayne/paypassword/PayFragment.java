a14655f77b655491c1c6534aee81e0e020a29f57
Dwayne <sink4u@163.com>
2018-03-14 22:07:35 +0800

    最终效果

---
+++ app/src/main/java/com/example/dwayne/paypassword/PayFragment.java
@@ -0,0 +1,17 @@
+package com.example.dwayne.paypassword;
+
+import android.support.v4.app.DialogFragment;
+
+
+/**
+ * @author Dwayne
+ * @email dev1024@foxmail.com
+ * @time 2018/3/13 21:13
+ * @change
+ * @chang time
+ * @class describe
+ */
+
+
+public class PayFragment extends DialogFragment {
+}
---
+++ app/src/main/java/com/example/dwayne/paypassword/PwdInputMethodView.java
@@ -0,0 +1,17 @@
+package com.example.dwayne.paypassword;
+
+import android.widget.LinearLayout;
+
+
+/**
+ * @author Dwayne
+ * @email dev1024@foxmail.com
+ * @time 2018/3/13 21:16
+ * @change
+ * @chang time
+ * @class describe
+ */
+
+
+public class PwdInputMethodView extends LinearLayout {
+}
